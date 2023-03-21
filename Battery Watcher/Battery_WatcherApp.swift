44821a2942c00c7fd29441064ec7ce0ba98641ca
Jake Meyer <20jmeyer@gmail.com>
2023-03-21 13:56:36 -0400

    Initial Commit

---
+++ Battery Watcher/Battery_WatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Battery_WatcherApp.swift
+//  Battery Watcher
+//
+//  Created by Jacob Meyer on 3/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Battery_WatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Battery Watcher/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Battery Watcher
+//
+//  Created by Jacob Meyer on 3/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
